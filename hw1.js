32572663d7efe78130b95857905da4df8c6ccce4
SeregeKEK <kurnakov2012@mail.ru>
2023-10-14 23:33:37 +0300

    hw

---
+++ hw1.js
@@ -0,0 +1,57 @@
+// 1) Дан массив const arr = [1, 5, 7, 9] с помощью Math.min и spread оператора,
+// найти минимальное число в массиве, решение задание должно состоять из одной
+// строки кода.
+
+arr = [1, 5, 7, 9];
+console.log(Math.min(...arr));
+
+// 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект
+// с двумя методами: increment и decrement. Метод increment должен увеличивать
+// значение счетчика на 1, а метод decrement должен уменьшать значение счетчика
+// на 1. Значение счетчика должно быть доступно только через методы объекта,
+// а не напрямую.
+
+function createCounter() {
+  let counter = 0;
+  return {
+    increment(number = 1) {
+      return (counter += number);
+    },
+    decrement(number = 1) {
+      return (counter -= number);
+    },
+  };
+}
+
+const Counter = createCounter();
+console.log(Counter.increment());
+console.log(Counter.decrement());
+console.log(Counter.decrement());
+
+// 3) Дополнительное задание, выполняем только если проходили работу с DOM.
+// Напишите рекурсивную функцию findElementByClass, которая принимает корневой
+// элемент дерева DOM и название класса в качестве аргументов и возвращает первый
+// найденный элемент с указанным классом в этом дереве.
+
+// Пример
+// const rootElement = document.getElementById('root');
+// const targetElement = findElementByClass(rootElement, 'my-class');
+// console.log(targetElement);
+
+const rootElement = document.getElementById("start");
+const targetElement = findElementByClass(rootElement, "cheburek");
+console.log(targetElement);
+
+function findElementByClass(root, myName) {
+  if (root.classList.contains(myName)) {
+    return root;
+  }
+  for (let i = 0; i < root.children.length; i++) {
+    const foundElement = findElementByClass(root.children[i], myName);
+    if (foundElement) {
+      return foundElement;
+    }
+  }
+  return null;
+}
+
